3a59071fdbb3eb76cec91d7c29573643fb7f861a
Eskey1 <116092442+Eskey1@users.noreply.github.com>
2023-09-23 15:15:42 +0300

    Initial commit

---
+++ src/test/java/com/example/Transport/schedule/TransportScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Transport.schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
