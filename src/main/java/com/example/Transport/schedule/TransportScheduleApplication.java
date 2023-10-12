6e8a9eb1fc178638ed0fff8a6e348eefed3fe150
Eskey1 <116092442+Eskey1@users.noreply.github.com>
2023-10-12 19:36:02 +0300

    Add registration

--- src/main/java/com/example/Transport/schedule/TransportScheduleApplication.java
+++ src/main/java/com/example/Transport/schedule/TransportScheduleApplication.java
@@ -2,8 +2,8 @@ package com.example.Transport.schedule;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -12,13 +12,9 @@ public class TransportScheduleApplication {
     public static void main(String[] args) {
         SpringApplication.run(TransportScheduleApplication.class, args);
     }
-    @GetMapping("/")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
-    }
-//    @GetMapping("/index")
-//    public String index() {
-//        return "index";
+//    @GetMapping("/")
+//    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+//        return String.format("Hello %s!", name);
 //    }
 }
 
