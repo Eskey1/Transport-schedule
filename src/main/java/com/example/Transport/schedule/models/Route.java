c32111f9f9d2717551638d36a520c8f56247e8b7
LoneGuardian1551 <danil.sarachev@yandex.ru>
2023-10-14 23:28:37 +0300

    add Tables1

--- src/main/java/com/example/Transport/schedule/models/Route.java
+++ src/main/java/com/example/Transport/schedule/models/Route.java
@@ -1,6 +1,9 @@
 package com.example.Transport.schedule.models;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -8,21 +11,15 @@ import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 @Entity
-@Table(name = "Shedule")
+@Table(name = "Route")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-
-public class Shedule {
-
+public class Route {
     @Id
     @GeneratedValue
-    private UUID SheduleId;
-
-    private String DepartureTime;
-
     private UUID RouteId;
 
-
+    private String RouteName;
 }
